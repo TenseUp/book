69ddec95f4f65166d65fced97bfc3736dd78f19e
SypherPK <46838766+TenseUp@users.noreply.github.com>
2023-02-24 11:22:32 +0800

    Upload

--- src/Author.java
+++ src/Author.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Author {
+public class Author {
+    private String name;
+    private String email;
+    private char gender;
+
+    public Author(String n, String e, char g){
+        this.name = n;
+        this.email = e;
+        this.gender = g;
+    }
+
+    public String getName(){return name;}
+    public String getEmail(){return email;}
+    public char getGender(){return gender;}
+    public void setEmail(String e){email = e;}
+    public String toString(){return name + " (" + gender + ") at " + email;}
 }
--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Book {
+public class Book {
+    private String name;
+    private Author author;
+    private double price;
+    private int qty;
+    public Book(String n, Author a, double p, int q){
+        this.name = n;
+        this.author = a;
+        this.price = p;
+        this.qty = q;
+    }
+    public String getName(){return name;}
+    public Author getAuthor(){return author;}
+    public double getPrice(){return price;}
+    public int getQty(){return qty;}
+    public void setPrice(double p){price = p;}
+    public void setQty(int q){qty = q;}
+    public String toString(){return "'"+name+"' by "+ getAuthor();}
 }
